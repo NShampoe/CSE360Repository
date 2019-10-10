07380cb79ceef948621e9fa500dc6411e934c7cd
Nick Shampoe <nshampoe@asu.edu>
2019-10-09 19:37:48 -0700

    Initial check in point for assignment two.

---
+++ CSE360/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,54 @@
+package cse360assign2;
+
+public class AddingMachine {
+	
+	private int total;
+/*
+ * Creates a new instance of this class, "AddingMachine".
+ * There is no return value.
+ * When created it instantiates the variable total to be 0.
+ * Doesn't have any input parameters.
+ */
+	public AddingMachine() {
+		total = 0;
+	}
+/*
+ * Returns a numerical integer value (in this version 0).
+ * Doesn't have any input parameters.
+ */
+	public int getTotal() {
+		return 0;
+	}
+/*
+ * There is no return value.
+ * 
+ * @param value   an integer value to be added to a sum that is stored
+ *                within this instance of AddingMachine.
+ */
+	public void add(int value) {
+		
+	}
+/*
+ * There is no return value.
+ * 
+ * @param value   an integer value to be subtracted from a sum that is stored
+ *                within this instance of AddingMachine.
+ */	
+	public void subtract(int value) {
+		
+	}
+/*
+ * (non-Javadoc)
+ * @see java.lang.Object#toString()
+ */
+	public String toString() {
+		return "";
+	}
+/*
+ * There is no return value.
+ * Doesn't have any input parameters.
+ */
+	public void clear() {
+		
+	}
+}
